85d6eb4dd772f17e441798b626c48ef96865925b
andrea-cle <andrea5750@hotmail.com>
2024-07-12 10:52:16 +0200

    "8e commit"

---
+++ js/gobal.js
@@ -0,0 +1,71 @@
+/*// Ceci est un commentaire
+// console.log("toto");
+
+// string
+
+let myVar = "ma variable";
+// let permet de modifier la valeur plus tard
+const myVar2 = "ma variable 2";
+// const ne permet pas de changer la valeur de la variable
+
+myVar = "variable qui change";
+// console.log(myVar);
+
+// boolean
+let isTrue = true;
+let isFalse = false;
+
+// console.log(isTrue);
+
+// number and operator
+let number = 1;
+let number2 = 2;
+// console.log(number, number2);
+
+// template string, littéraux de gabarits et concat
+
+let test = "test" + myVar + "value";
+let test2 = `test ${myVar}`;
+// littéraux de gabarit avec le $
+// console.log(typeof test2);
+
+// Conditions
+
+if (number === 10) {
+  console.log("la condition est validée");
+} else if (number <= 10) {
+  console.log("je passe là");
+} else {
+  console.log("condition pas valide");
+}
+
+// Array
+
+let array = [1, 2, 4];
+console.log(array[2]);
+
+// Objet
+
+const objet = {
+  age: 36,
+  title: "Ma description",
+};
+console.log(objet.title, objet.age);
+
+// Boucle : while, for, foreach
+
+// for (let i = 0; i < array.length; i++) {
+//   console.log(array[i]);
+// }
+
+// array.forEach((item) => {
+//   console.log(item);
+// });
+
+// Functions
+
+const myFunction = (item) => {
+  console.log(item);
+};*/
+
+myFunction("toto");
